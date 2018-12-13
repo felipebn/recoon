e074443a43504f2f9151402f20af7686c4a4b462
Felipe Nascimento <thrash.nation@gmail.com>
2018-12-13 23:26:38 +0000

    Implementing single opening workflow view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import JobOpenings from './jobOpening/JobOpenings'
+import SingleJobOpening from './jobOpening/SingleJobOpening'
 
 //TODO routing
 class App extends Component {
   render() {
     return (
-      <JobOpenings/>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={JobOpenings}/>
+          <Route path='/jobopening/:id' component={SingleJobOpening}/>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ frontend/src/jobOpening/SingleJobOpening.js
@@ -0,0 +1,89 @@
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import React, { Component } from 'react';
+
+class SingleJobOpening extends Component {
+  render() {
+    var opening = this.props.opening;
+    return (
+      <div className="container">
+        <h1 className="title">{opening.name}</h1>
+        <h2 className="subtitle">{opening.workflow.name}</h2>
+
+        <div className="columns is-multiline is-mobile">
+          {opening.workflow.stages.map(workflowStage => <WorkflowStageColumn stage={workflowStage}/>)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class WorkflowStageColumn extends Component {
+  render() {
+    var stage = this.props.stage;
+
+    return (
+      <div className="column recoon-stage-column">
+        <div className="stage-top-border"></div>
+        <div className="stage-contents">
+          <h2 className="title is-size-5">{stage.name}</h2>
+          {stage.candidates.map(c => this.renderApplicantCard(c))}     
+        </div>
+      </div>
+    );
+  }
+
+  renderApplicantCard(candidate){
+    return (
+      <div className="card stage-card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="https://ui-avatars.com/api/?background=0D8ABC&color=fff"/>
+              </figure>
+            </div>
+            <div className="media-content">
+              <p className="title is-5">{candidate.name}</p>
+            </div>
+          </div>      
+        </div>
+      </div>
+    );
+  }
+}
+
+const GET_JOBOPENING = gql`
+  {
+    jobOpening(id:1){
+      id,
+      name,
+      applicantCount,
+      responsible,
+      workflow{
+        id,
+        name,
+        stages {
+          id, name, order, candidates {
+            id, name
+          }
+        }
+      }
+    }
+  }
+`;
+
+const SingleJobOpeningQuery = () => (
+  <Query query={GET_JOBOPENING}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <SingleJobOpening opening={data.jobOpening}/>
+      );
+    }}
+  </Query>
+);
+
+export default SingleJobOpeningQuery;
