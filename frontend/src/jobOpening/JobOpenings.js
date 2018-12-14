3932a604237f85ab426e869d6db56d526ca7379c
Felipe Nascimento <thrash.nation@gmail.com>
2018-12-14 00:50:36 +0000

    Making job openings on overview clickable

--- frontend/src/jobOpening/JobOpenings.js
+++ frontend/src/jobOpening/JobOpenings.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
 
 class JobOpenings extends Component {
   render() {
@@ -21,25 +22,28 @@ class JobOpeningCard extends Component {
     var opening = this.props.opening;
 
     return (
-      <div className="column is-one-third">
-        <div className="box recoon-card">
-          <h1>{opening.name}</h1>
-          <div className="field is-grouped is-grouped-multiline recoon-card-tags">
-            <div className="control">
-              <div className="tags has-addons">
-                <span className="tag is-dark">Applicants</span>
-                <span className="tag is-info">{opening.applicantCount}</span>
+      
+        <div className="column is-one-third">
+          <div className="box recoon-card">
+          <Link to={`/jobopening/${opening.id}`} className="no-style">
+            <h1>{opening.name}</h1>
+            <div className="field is-grouped is-grouped-multiline recoon-card-tags">
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Applicants</span>
+                  <span className="tag is-info">{opening.applicantCount}</span>
+                </div>
+              </div>
+              <div className="control">
+                <div className="tags has-addons">
+                  <span className="tag is-dark">Responsible</span>
+                  <span className="tag is-success">{opening.responsible}</span>
+                </div>
               </div>
             </div>
-            <div className="control">
-              <div className="tags has-addons">
-                <span className="tag is-dark">Responsible</span>
-                <span className="tag is-success">{opening.responsible}</span>
-              </div>
-            </div>
+          </Link>
           </div>
         </div>
-      </div>
     );
   }
 }
